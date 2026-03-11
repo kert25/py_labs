1a70570c735bfe55f04bf04faefbd84cf7fdcdb1
kert25 <kirill.laukert.me@gmail.com>
2026-03-11 19:50:21 +0300

    Сделал задание повышенного уровня 9

--- lab2/mid_6.py
+++ lab2/mid_6.py
@@ -3,30 +3,30 @@ print("--- Калькулятор (введите 'выход' для завер
 while True:
     try:
         action = input("Выберите операцию (+, -, *, /) или 'выход': ").lower()
-        
-        if action == 'выход':
+
+        if action == "выход":
             print("Программа завершена.")
             break
 
-        if action not in ('+', '-', '*', '/'):
+        if action not in ("+", "-", "*", "/"):
             print("Ошибка: Неверная операция!")
             continue
 
         num1 = float(input("Первое число: "))
         num2 = float(input("Второе число: "))
 
-        if action == '+':
+        if action == "+":
             print(f"Результат: {num1 + num2}")
-        elif action == '-':
+        elif action == "-":
             print(f"Результат: {num1 - num2}")
-        elif action == '*':
+        elif action == "*":
             print(f"Результат: {num1 * num2}")
-        elif action == '/':
+        elif action == "/":
             if num2 != 0:
                 print(f"Результат: {num1 / num2}")
             else:
                 print("Ошибка: Деление на ноль!")
-        
+
         print("-" * 20)
 
     except ValueError:
