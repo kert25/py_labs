7ea8a6d75f6408e24599d63a93d8ccdd064be745
kert25 <kirill.laukert.me@gmail.com>
2026-03-27 20:48:06 +0300

    lab4: add mid_2.py

---
+++ lab4/mid_2.py
@@ -0,0 +1,6 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
+
+print(f"Исходный список: {numbers}")
+print(f"Чётные числа: {even_numbers}")
